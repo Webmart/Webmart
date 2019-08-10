1513f11ec88915c737bc325bac742e53c817ac65
George Kary <geogkary@gmail.com>
2019-08-10 11:33:57 +0300

    Create wm.php

---
+++ wm.php
@@ -0,0 +1,16 @@
+<?php
+
+/*!
+* Webmart 2.0
+* A simple PHP framework for building web applications and websites.
+* https://github.com/Webmart
+*/
+
+define('WM_THEME', ''); /** Set a theme folder */
+define('WM_DEBUG', false); /** Enable debug mode */
+define('WM_BASE', ''); /** Set the base URL */
+
+define('WM_SITEMAP', false); /** Auto-generate a sitemap */
+define('WM_ROBOTS', false); /** Auto-generate a robots file */
+
+define('WM_HTTPS', false); /** Force HTTPs */
