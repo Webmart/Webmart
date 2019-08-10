1ee8c4342f9728208a7386b8c97e0a5ba27f8c39
George Kary <geogkary@gmail.com>
2019-08-10 11:33:55 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*!
+* Webmart 2.0
+* A simple PHP framework for building web applications and websites.
+* https://github.com/Webmart
+*/
+
+require 'wm.php';
+require 'engine/core/Webmart.php';
+
+Webmart::init();
