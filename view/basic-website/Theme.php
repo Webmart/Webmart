4634bcdb2f1bd245569143127b47a7c654c5ef02
George Kary <geogkary@gmail.com>
2019-09-15 17:50:38 +0300

    Include basic-website files

---
+++ view/basic-website/Theme.php
@@ -0,0 +1,21 @@
+<?php
+
+/*!
+* Webmart 2.0
+* A simple PHP framework for building web applications and websites.
+* https://github.com/Webmart
+*/
+
+class Theme
+{
+
+    /**
+    * @method
+    */
+
+    public function __construct($route)
+    {
+        
+    }
+
+}
