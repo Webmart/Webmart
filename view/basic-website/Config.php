c77a6a7399c745cd23a6cc6d000c361225c09c6e
George Kary <geogkary@gmail.com>
2019-09-21 22:33:01 +0300

    Update Config.php

--- view/basic-website/Config.php
+++ view/basic-website/Config.php
@@ -1,29 +1,35 @@
 <?php
 
 /*!
-* Webmart 2.0
-* A simple PHP framework for building web applications and websites.
-* https://github.com/Webmart
+* Webmart
+* A basic PHP framework for web applications and websites.
+* https://webmartphp.com/
 */
 
 class Config
 {
 
+    /** VERSION theme */
+
     public static $version = '1.0';
 
-    /** COOKIES to accept */
+    /** COOKIES data to accept */
 
     public static $cookies = array();
 
     /** GET data to accept */
 
-    public static $query = array();
+    public static $query = array(
+        'utm_source',
+        'medium',
+        'ref'
+    );
 
     /** POST data to accept */
 
     public static $data = array();
 
-    /** Database access */
+    /** DATABASE configuration (requires Medoo library) */
 
     public static $db = array(
         'database_type' => '',
@@ -33,34 +39,39 @@ class Config
         'password' => ''
     );
 
-    /** Set your Flight redirects. */
+    /** FLIGHT redirect requests to pages with an HTTP response */
 
     public static $redirects = array(
         '301' => array(
-            '/home' => '/'
+            '/home' => '/',
+            '/about-us' => '/about/',
+            '/legal/terms-of-service' => '/legal/terms/'
         )
     );
 
-    /** Set your Flight routes. */
+    /** FLIGHT route pages with dynamic parameters */
 
     public static $routes = array(
-        'example-(subpage-(finalpage))'
+        'about', // absolute rule
+        'legal-(subpage)' // dynamic rule, ex. legal/privacy/, legal/terms/
     );
 
-    /** Disallow pages to be indexed - robots.txt **/
+    /** SEO disallow pages to be indexed on robots.txt **/
 
     public static $noindex = array(
-        'home'
+        'home',
+        'about-us',
+        'legal/terms-of-service'
     );
-      
-    /** Load Google Hosted Libraries */
+
+    /** GOOGLE load supported libraries */
 
     public static $googlelibs = array(
         'jquery' => '3.4.1',
         'maps' => 'API_KEY'
     );
 
-    /** Load Google fonts */
+    /** GOOGLE load fonts with weights and subsets */
 
     public static $googlefonts = array(
         'Fira Sans' => array(
