6c14230792796aea1a66a475a143eefd8ce9b735
George Kary <geogkary@gmail.com>
2019-09-21 15:30:52 +0300

    New libs/index.php

---
+++ engine/libs/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*!
+* Webmart
+* A basic PHP framework for web applications and websites.
+* https://webmartphp.com/
+*/
+
+/** Add your PHP libraries here, ex. Medoo.php, Mobile_Detect.php, etc */
