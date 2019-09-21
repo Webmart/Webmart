2f4daaad6f28fd4117cd818e8dcc43742c6d4442
George Kary <geogkary@gmail.com>
2019-09-21 22:52:28 +0300

    Minor fix

--- view/basic-website/templates/header.php
+++ view/basic-website/templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang version=<?php echo $vars->version ?>>
+<html lang version="<?php echo $vars->version ?>">
     <head>
         <title><?php echo $vars->title ?></title>
         <meta name="description" content="<?php echo $vars->description ?>" />
