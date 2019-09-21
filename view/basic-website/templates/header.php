4269bdb8c2315da31cd3e94cd7aeb0cdfdd1fcc4
George Kary <geogkary@gmail.com>
2019-09-21 22:52:07 +0300

    Update header.php

---
+++ view/basic-website/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang version=<?php echo $vars->version ?>>
+    <head>
+        <title><?php echo $vars->title ?></title>
+        <meta name="description" content="<?php echo $vars->description ?>" />
+    </head>
+    <body>
+        <header></header>
+        <main>
