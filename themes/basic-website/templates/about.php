5c6e519f3059648dbf124a0c4e19e55397ee74c9
George Kary <geogkary@gmail.com>
2019-10-28 20:27:40 +0200

    Update about.php

--- themes/basic-website/templates/about.php
+++ themes/basic-website/templates/about.php
@@ -3,4 +3,4 @@
 echo '<h1>' . $heading . '</h1>';
 echo '<h2>Current page is ' . $page . '</h2>';
 echo '<h2>Current template is ' . $template . '</h2>';
-echo '<p>' . $welcome . '</p>';
+echo '<p>' . $urls['page'] . '</p>';
